83c8bc9b1897cdc67e86e5abd138089d09912362
MediaDUK <hello@mediaduk.com>
2018-10-05 23:59:24 -0700

    audio added on keyPress event

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # 30-days-js
 
 > react, webpack, babel
-
-
--- src/js/components/presentational/DayOne.js
+++ src/js/components/presentational/DayOne.js
@@ -1,63 +1,83 @@
-import React, { Component } from 'react'
-import guitarOne from '../../../audio/1-guitar.mp3'
-
+import React, { Component } from "react";
+import Key from "../container/Key";
+import guitarOne from "../../../audio/1-guitar.mp3";
+import guitarTwo from "../../../audio/2-guitar.mp3";
+import guitarThree from "../../../audio/3-guitar.mp3";
+import guitarFour from "../../../audio/4-guitar.mp3";
+import kick from "../../../audio/5-kick.mp3";
+import clap from "../../../audio/6-clap.mp3";
 class DayOne extends Component {
   constructor() {
     super();
     this.state = {
-      title: "",
-      description: ""
+      title: "Day #1 Drum Machine",
+      description:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt blanditiis doloremque quaerat"
     };
     this.keyPressed = this.keyPressed.bind(this)
     this.play = this.play.bind(this)
+
     this.guitarOne = new Audio(guitarOne)
+    this.guitarTwo = new Audio(guitarTwo)
+    this.guitarThree = new Audio(guitarThree)
+    this.guitarFour = new Audio(guitarFour)
+    this.kick = new Audio(kick)
+    this.clap = new Audio(clap)
     // https://www.w3schools.com/tags/ref_av_dom.asp
   }
-  play(key) {
-    if (key = 65) {
-      this.guitarOne.play()
-    }
+  play(audioFile) {
+    const audio = audioFile
+    audio.currentTime = 0
+    audio.play()
   }
   keyPressed(e) {
     const key = e.keyCode
+    // const keyCode = e.keyCode? e.keyCode:
+    console.log(`keyPressed(${key})`)
     switch (key) {
       case 65:
-        this.play(65)
-        break;
+        this.play(this.guitarOne)
+        break
+      case 83:
+        this.play(this.guitarTwo)
+        break
+      case 68:
+        this.play(this.guitarThree)
+        break
+      case 70:
+        this.play(this.guitarFour)
+        break
+      case 71:
+        this.play(this.kick)
+        break
+      case 72:
+        this.play(this.clap)
+        break        
       default:
-        break;
+        break
     }
-
   }
   componentWillMount() {
-    document.addEventListener('keydown', this.keyPressed, true);
+    document.addEventListener('keydown', this.keyPressed, true)
   }
 
   componentWillUnmount() {
-    document.removeEventListener('keydown', this.keyPressed, true);
+    document.removeEventListener('keydown', this.keyPressed, true)
   }
 
   render() {
     return (
       <section className="day">
-        <h2>Day #1</h2>
-    
+        <h2>{this.state.title}</h2>
+        <p>{this.state.description}</p>
         <div className="keys">
-          <div data-key="65" className="key">
-            <kbd>A</kbd>
-            <span className="sound">Guitar 1</span>
-          </div>
+          <Key keyCode={65} keyLetter={"A"} name={"Guitar 1"} />
+          <Key keyCode={83} keyLetter={"S"} name={"Guitar 2"} />
+          <Key keyCode={68} keyLetter={"D"} name={"Guitar 3"} />
+          <Key keyCode={70} keyLetter={"F"} name={"Guitar 4"} />
+          <Key keyCode={71} keyLetter={"G"} name={"Kick"} />
+          <Key keyCode={72} keyLetter={"H"} name={"Clap"} />
         </div>
-
-        <audio data-key="65" src="" />
-        <audio data-key="83" src="" />
-        <audio data-key="68" src="" />
-        <audio data-key="70" src="" />
-        <audio data-key="71" src="" />
-        <audio data-key="72" src="" />
-        <audio data-key="74" src="" />
-        <audio data-key="75" src="" />
-        <audio data-key="76" src="" />
       </section>
     );
   }
