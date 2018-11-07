fc9f977bfcc79fdc385990a809a69d480f126769
MediaDUK <hello@mediaduk.com>
2018-11-06 23:39:39 -0800

    bs4 added

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import Header from './components/presentational/Header';
-import DayOne from './components/presentational/DayOne';
-import DayTwo from './components/presentational/DayTwo';
+import jquery from "jquery/dist/jquery.slim";
+import Popper from "popper.js/dist/popper";
+import "bootstrap/dist/js/bootstrap";
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Header from "./components/presentational/Header";
+import DayOne from "./components/presentational/DayOne";
+import DayTwo from "./components/presentational/DayTwo";
 
 function Index() {
   return (
     <div className="container">
       <Header />
+      <div className="card">
+        {/* <img className="card-img-top"  alt="Card image cap"> */}
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+          <a href="#" className="btn btn-primary">
+            Go somewhere
+          </a>
+        </div>
+      </div>
       <Router>
         <main>
           <nav>
@@ -25,4 +41,4 @@ function Index() {
   );
 }
 
-ReactDOM.render(<Index />, document.getElementById('app'));
+ReactDOM.render(<Index />, document.getElementById("app"));
--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,21 @@
-const path = require('path');
-const HtmlWebPackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   // https://webpack.js.org/configuration/target/
   // see target later and experiment with front and back end builds
-  target: 'web',
+  target: "web",
   // set to none and let npm scripts pass w/ CLI --mode
-  mode: 'none',
+  mode: "none",
   // webpack-dev-server will monitor the code dependency
   // of these entry points, and re-create the bundle
   // when changes are detected.
-  entry: [
-    './src/js/index.js',
-    './src/sass/main.sass',
-    './src/index.html'],
+  entry: ["./src/js/index.js", "./src/sass/main.sass", "./src/index.html"],
   output: {
-    path: path.resolve(__dirname, 'public'),
-    filename: 'js/[name].js',
+    path: path.resolve(__dirname, "public"),
+    filename: "js/[name].js"
   },
-  devtool: 'inline-source-map',
+  devtool: "inline-source-map",
   // regex statement to identify the JavaScript files to be transpiled
   // with the babel-loader (except node_modules). Babel-loader is told
   // to use the babel-preset-env (transpile parameters .babelrc)
@@ -26,98 +23,97 @@ module.exports = {
     rules: [
       {
         test: /\.js$/,
-        exclude: /(node_modules)/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
-            presets: ['babel-preset-react'],
-          },
-        },
+            presets: ["babel-preset-react"]
+          }
+        }
       },
       {
         test: /\.sass$/,
         use: [
           {
-            loader: 'file-loader',
+            loader: "file-loader",
             options: {
-              name: 'css/[name].css',
-            },
+              name: "css/[name].css"
+            }
           },
           {
-            loader: 'extract-loader',
+            loader: "extract-loader"
           },
           {
             // parses the CSS into JavaScript and resolves any dependencies
-            loader: 'css-loader?-url',
+            loader: "css-loader?-url"
           },
           {
-            loader: 'postcss-loader',
+            loader: "postcss-loader",
             options: {
               config: {
-                path: './postcss.config.js',
-              },
-            },
+                path: "./postcss.config.js"
+              }
+            }
           },
           {
-            loader: 'sass-loader',
-          },
-        ],
+            loader: "sass-loader"
+          }
+        ]
       },
       {
         test: /\.html$/,
         use: [
           {
-            loader: 'html-loader',
+            loader: "html-loader",
             options: {
-              minimize: true,
-            },
-          },
-        ],
+              minimize: true
+            }
+          }
+        ]
       },
       {
         test: /\.(png|jpg|gif)$/i,
         use: [
           {
-            loader: 'url-loader',
+            loader: "url-loader",
             options: {
               limit: 8192,
-              name: 'img/[name].[ext]',
-            },
-          },
-        ],
+              name: "img/[name].[ext]"
+            }
+          }
+        ]
       },
       {
         test: /\.mp3$/,
-        loader: 'file-loader',
+        loader: "file-loader",
         query: {
-          name: 'audio/[name].[ext]',
-        },
-      },
-    ],
+          name: "audio/[name].[ext]"
+        }
+      }
+    ]
   },
   plugins: [
     new HtmlWebPackPlugin({
-      template: './src/index.html',
-      filename: './index.html',
-    }),
+      template: "./src/index.html",
+      filename: "./index.html"
+    })
   ],
   devServer: {
     // host can be omitted unless you are using 'docker'
     // host: '0.0.0.0',
-    clientLogLevel: 'info',
+    clientLogLevel: "info",
     compress: true,
-    port: 9000,
+    port: 8080,
     watchContentBase: true,
     headers: {
-      'X-Custom-Header': 'yes',
+      "X-Custom-Header": "yes"
     },
     stats: {
-      colors: true,
-    },
+      colors: true
+    }
     // https: {
     //   cert: fs.readFileSync('path-to-cert-file.pem'),
     //   key: fs.readFileSync('path-to-key-file.pem'),
     //   cacert: fs.readFileSync('path-to-cacert-file.pem')
     // }
-  },
+  }
 };
